d5e28f53424a1691e7924b7a4bb5df44f208c478
Lars Steen <lats@aarhus.dk>
2019-03-21 10:20:53 +0100

    Only admin users should create surveys for other users

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use AlterPHP\EasyAdminExtensionBundle\Controller\EasyAdminController as BaseAdminController;
+use EasyCorp\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
 
 class AdminController extends BaseAdminController
 {
@@ -21,5 +22,40 @@ class AdminController extends BaseAdminController
     {
         $this->get("fos_user.user_manager")->updateUser($user, false);
         parent::updateEntity($user);
+    }
+
+    public function createSurveyNewForm($entity, $fields)
+    {
+        $form = parent::createNewForm($entity, $fields);
+
+        // We remove fields from the form if the currently logged in
+        // user is not allowed to set a value for a specific field.
+        foreach ($fields as $name => $field)
+        {
+            if ( empty($field['role']) )
+            {
+                continue;
+            }
+
+            if ( ! $this->isGranted($field['role']) )
+            {
+                $form->remove($name);
+            }
+        }
+
+        return $form;
+    }
+
+    public function persistSurveyEntity($entity)
+    {
+        // Making sure there always is a user attached to the Survey.
+        // If the currently logged in User is not an admin, no User will be attached before now
+        // due to the User field in the form for creating a Survey is only showed to admins.
+        if ( empty($entity->getUser()) )
+        {
+            $entity->setUser($this->getUser());
+        }
+
+        parent::persistEntity($entity);
     }
 }
