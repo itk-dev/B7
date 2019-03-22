571387e48d3580418e87bbd007fb915ded77fd26
Lars Steen <lats@aarhus.dk>
2019-03-22 08:07:22 +0100

    Fixed errors reported in code quality tools

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,18 +8,18 @@ class AdminController extends BaseAdminController
 {
     public function createNewUserEntity()
     {
-        return $this->get("fos_user.user_manager")->createUser();
+        return $this->get('fos_user.user_manager')->createUser();
     }
 
     public function persistUserEntity($user)
     {
-        $this->get("fos_user.user_manager")->updateUser($user, false);
+        $this->get('fos_user.user_manager')->updateUser($user, false);
         parent::persistEntity($user);
     }
 
     public function updateUserEntity($user)
     {
-        $this->get("fos_user.user_manager")->updateUser($user, false);
+        $this->get('fos_user.user_manager')->updateUser($user, false);
         parent::updateEntity($user);
     }
 }
